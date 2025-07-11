0772c3835c63dfc5b0480007112b7272f5080fcd
fedi-nabli <fedinabli@gmail.com>
2025-07-11 17:13:40 +0200

    Updated sum

--- Formula/synapse.rb
+++ Formula/synapse.rb
@@ -3,7 +3,7 @@ class Synapse < Formula
   homepage "https://github.com/fedi-nabli/SynapseEngineBinaries"
   version "0.1.5"
 
-  url "https://github.com/fedi-nabli/SynapseEngineBinaries/blob/main/macos/synapse"
+  url "https://github.com/fedi-nabli/SynapseEngineBinaries/raw/main/macos/synapse"
   sha256 "0bc2b0fbcec8a39c2c0e5bc482bbed4e9b61904c10bafc4c6f476ac28204482e"
 
   def instal
