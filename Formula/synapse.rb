863042bf71d946cb9d0f7f4c9a0887ecc51658be
fedi-nabli <fedinabli@gmail.com>
2025-07-11 17:22:12 +0200

    updated binary version

--- Formula/synapse.rb
+++ Formula/synapse.rb
@@ -4,7 +4,7 @@ class Synapse < Formula
   version "0.1.5"
 
   url "https://github.com/fedi-nabli/SynapseEngineBinaries/raw/main/macos/synapse"
-  sha256 "0bc2b0fbcec8a39c2c0e5bc482bbed4e9b61904c10bafc4c6f476ac28204482e"
+  sha256 "c29d02675d67c9258e9f88f571a602928695063e818b18876731e0a6727fa40d"
 
   def install
     bin.install "synapse"
