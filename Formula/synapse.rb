2a28eef64661a3fd1261e3eb39628bcac603102c
fedi-nabli <fedinabli@gmail.com>
2025-07-11 17:09:29 +0200

    Updated correct repo path

--- Formula/synapse.rb
+++ Formula/synapse.rb
@@ -1,9 +1,9 @@
 class Synapse < Formula
   desc "Synapse Engine CLI Tool"
-  homepage "https://github.com/fedi-nabli/homebrew-synapse"
+  homepage "https://github.com/fedi-nabli/SynapseEngineBinaries"
   version "0.1.5"
 
-  url "https://github.com/fedi-nabli/homebrew-synapse/macos/synapse"
+  url "https://github.com/fedi-nabli/SynapseEngineBinaries/blob/main/macos/synapse"
   sha256 "0bc2b0fbcec8a39c2c0e5bc482bbed4e9b61904c10bafc4c6f476ac28204482e"
 
   def instal
