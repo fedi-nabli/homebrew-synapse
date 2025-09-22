d4904129d48a275407a7326ffa064a1f399d0f31
fedi-nabli <fedinabli@gmail.com>
2025-09-23 00:30:17 +0100

    Uploaded version 0.2.0

--- Formula/synapse.rb
+++ Formula/synapse.rb
@@ -1,7 +1,7 @@
 class Synapse < Formula
   desc "Synapse Engine CLI Tool"
   homepage "https://github.com/fedi-nabli/SynapseEngineBinaries"
-  version "0.1.5"
+  version "0.2.0"
 
   url "https://github.com/fedi-nabli/SynapseEngineBinaries/raw/main/macos/synapse"
   sha256 "c29d02675d67c9258e9f88f571a602928695063e818b18876731e0a6727fa40d"
