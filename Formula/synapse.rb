5e1fd8fdf7807cd2b072bada826777a8895111b8
Fedi Nabli <64886531+fedi-nabli@users.noreply.github.com>
2025-10-25 21:23:58 +0100

    Bump Synapse version from 0.2.5 to 0.3.0

--- Formula/synapse.rb
+++ Formula/synapse.rb
@@ -1,7 +1,7 @@
 class Synapse < Formula
   desc "Synapse Engine CLI Tool"
   homepage "https://github.com/fedi-nabli/SynapseEngineBinaries"
-  version "0.2.5"
+  version "0.3.0"
 
   url "https://github.com/fedi-nabli/SynapseEngineBinaries/raw/main/macos/synapse"
   sha256 "9fba4b8053ce9f48283851d82692f90b0748461ffa57beba8608d4661cc78995"
